fd814bafde25c655f4cce479371659064ac90647
AlyfAiman <m.alyfaiman@gmail.com>
2021-06-21 10:34:32 +0800

    Dimensional Shift Variants
    - Added Timer to dimensional shift
    - Auto & Manual Shift variants

--- GPS1_BeyondTwoBoundaries/Assets/Scripts/DimensionShift.cs
+++ GPS1_BeyondTwoBoundaries/Assets/Scripts/DimensionShift.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class DimensionShift : MonoBehaviour
 {
     
     PlatformerMovement playerMovement;
+
+    public bool ManualShift;
+    public bool AutoShfit;
+    
+
     public bool ghostState = false;  
 
     public GameObject MainPlayer;
@@ -18,17 +25,45 @@ public class DimensionShift : MonoBehaviour
     private GameObject playerTracking;
 
     public GameObject[] ghostWalls;
-    
+
+    //Timer Stuff
+    public float TimerDuration = 10f;
+    public float timeStart;
+    public Text textBox;
+    public bool timerActive = false;
+
+    public Image SoulBar;
+    public Image filter;
 
     void Awake()
     {
+        textBox.text = timeStart.ToString("F2");
         
+        //textBox.text = timeStart.ToString();
     }
 
     
     void Update()
     {
         ShiftDimension();
+
+        
+
+
+        if (timerActive)
+        {
+            timeStart -= Time.deltaTime;
+            textBox.text = timeStart.ToString("F2");
+            //textBox.text = Mathf.Round(timeStart).ToString();
+        }
+        else
+        {
+            timeStart = 0;
+            textBox.text = timeStart.ToString("F2");
+        }
+
+        SoulBar.fillAmount = timeStart / TimerDuration;
+        
     }
 
     public void ShiftDimension()
@@ -40,61 +75,149 @@ public class DimensionShift : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            if (!ghostState)
+            if (!ghostState && (AutoShfit || ManualShift))
             {
-                ghostState = true;
-
-
-                //Set decoy position at main player position
-                Decoy.transform.position = new Vector2(playerTracking.transform.position.x, playerTracking.transform.position.y); 
-
-                //Set ghost player position at main player position 
-                GhostPlayer.transform.position = new Vector2(playerTracking.transform.position.x, playerTracking.transform.position.y);
-
-                //Disable main player
-                MainPlayer.SetActive(false);  
-
-                //Enable decoy
-                Decoy.SetActive(true);
-
-                //Enable ghost player
-                GhostPlayer.SetActive(true);
-
-                //Reveal Ghost Walls
-                ghostWalls[0].SetActive(false);
-                ghostWalls[1].SetActive(false);
-                ghostWalls[2].SetActive(true);// change later (this is filter)
+                ShiftToSoul();
 
             }
-            if(ghostState && ghostMove.isTouchingDecoy)
+            else if(ghostState && ManualShift)
             {
-                
-                ghostState = false;
-
-                //Set main player position to decoy position
-                MainPlayer.transform.position = new Vector2(decoyTracking.transform.position.x, decoyTracking.transform.position.y);
-
-                //Disable ghost player
-                GhostPlayer.SetActive(false);
-
-                //Disable decoy
-                Decoy.SetActive(false);
-
-                //Hide ghost walls
-                ghostWalls[0].SetActive(true);
-                ghostWalls[1].SetActive(true);
-                ghostWalls[2].SetActive(false); 
-
-                //Enable main player
-                MainPlayer.SetActive(true);
-                
-                
-                
+                if (ghostMove.isTouchingDecoy)
+                {
+                    ManualShiftToBody();
+                }
+                         
+            }
+            else if(ghostState && AutoShfit)
+            {
+                if (!ghostMove.isTouchingDecoy)
+                {
+                    AutoShiftToBody();
+                }
 
             }
         }
+
+        if(ghostState && ManualShift)
+        {
+            if(timeStart <= 0)
+            {      
+                SceneManager.LoadScene("Alyf's Scene");
+            }
+        }
+
+        if(ghostState && AutoShfit)
+        {
+            if (timeStart <= 0)
+            {
+                AutoShiftToBody();
+            }
+        }
+
+
               
     }
 
+    public void ShiftToSoul()
+    {
+        ghostState = true;
 
+
+        //Set decoy position at main player position
+        Decoy.transform.position = new Vector2(playerTracking.transform.position.x, playerTracking.transform.position.y);
+
+        //Set ghost player position at main player position 
+        GhostPlayer.transform.position = new Vector2(playerTracking.transform.position.x, playerTracking.transform.position.y);
+
+        //Disable main player
+        MainPlayer.SetActive(false);
+
+        //Enable decoy
+        Decoy.SetActive(true);
+
+        //Enable ghost player
+        GhostPlayer.SetActive(true);
+
+        //Reveal Ghost Walls
+        ghostWalls[0].SetActive(false);
+        ghostWalls[1].SetActive(false);
+        ghostWalls[2].SetActive(true);// change later (this is filter)
+
+        //Start Timer
+        SetTimer();
+    }
+
+    public void ManualShiftToBody()
+    {
+        ghostState = false;
+
+        //Set main player position to decoy position
+        MainPlayer.transform.position = new Vector2(decoyTracking.transform.position.x, decoyTracking.transform.position.y);
+
+        //Disable ghost player
+        GhostPlayer.SetActive(false);
+
+        //Disable decoy
+        Decoy.SetActive(false);
+
+        //Hide ghost walls
+        ghostWalls[0].SetActive(true);
+        ghostWalls[1].SetActive(true);
+        ghostWalls[2].SetActive(false);
+
+        //Enable main player
+        MainPlayer.SetActive(true);
+
+        //Reset Filter
+        filter.canvasRenderer.SetAlpha(0);
+
+        //Stop Timer
+        SetTimer();
+    }
+
+    public void AutoShiftToBody()
+    {
+        
+
+        ghostState = false;  
+
+        //Set main player position to decoy position
+        MainPlayer.transform.position = new Vector2(decoyTracking.transform.position.x, decoyTracking.transform.position.y);
+
+        //Disable ghost player
+        GhostPlayer.SetActive(false);
+
+        //Disable decoy
+        Decoy.SetActive(false);
+
+        //Hide ghost walls
+        ghostWalls[0].SetActive(true);
+        ghostWalls[1].SetActive(true);
+        ghostWalls[2].SetActive(false);
+
+        //Enable main player
+        MainPlayer.SetActive(true);
+
+        //Reset Filter
+        filter.canvasRenderer.SetAlpha(0);
+
+        //Stop Timer
+        SetTimer();
+
+    }
+
+    public void SetTimer()
+    { 
+        timerActive = !timerActive;
+        timeStart = TimerDuration;
+        Debug.Log ("Timer Called");
+    }
+
+    public void FilterFade()
+    {
+        filter.canvasRenderer.SetAlpha(timeStart / TimerDuration);
+
+    }
+
+    
 }
